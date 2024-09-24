db3f1ff38d62de6061c2dc63b7b529f711505ee3
Gauthier Detroussel <detrous231@etu.unicaen.fr>
2024-09-24 12:49:41 +0200

    choix des durees + affichage basique

--- js/script.js
+++ js/script.js
@@ -1,41 +1,76 @@
-let tempsTravail = 25
-let tempsPause = 5
-let seconde = tempsTravail*60
-let bool = true
-let affMin = document.getElementById("min")
-let affSeconde = document.getElementById("sec")
-let but = document.getElementById("tonGrosBouton")
+let tempsTravail = 25; 
+let tempsPause = 5;    
+let seconde = 1;
+let bool = true;
+let affMin = document.getElementById("min");
+let affSeconde = document.getElementById("sec");
+let but = document.getElementById("tonGrosBouton");
+let tempsBool = true;
+const parametres = document.getElementById("param");
+const inputTravail = document.getElementById("tpsTravail"); 
+const inputPause = document.getElementById("tpsPause"); 
+const confirmBtn = document.getElementById("confirmBtn");
 
-console.log("oui")
+affMin.textContent = tempsTravail;
+affSeconde.textContent = "00";
 
-function lancer(){
-    if(bool == true){
-        lIntervalDuBoss = window.setInterval(deffilage, 1000)
-        bool = false
+function lancer() {
+    if (bool === true) {
+        seconde = tempsTravail * 60;
+        lIntervalDuBoss = window.setInterval(deffilage, 1); 
+        bool = false;
     }
-    but.textContent = "Stauopp"
-    but.onclick = reset
+    but.textContent = "Stop";
+    but.onclick = reset;
 }
 
-
-function deffilage(){
-    if(seconde > 0){
-        seconde--
-        min = Math.floor(seconde/60)
-        affMin.textContent = min
-        affSeconde.textContent = seconde%60
+function deffilage() {
+    if (seconde > 0) {
+        seconde--;
+        let min = Math.floor(seconde / 60);
+        let sec = seconde % 60;
+        affMin.textContent = min;
+        affSeconde.textContent = sec < 10 ? "0" + sec : sec; 
+    } else {
+        if (tempsBool === true) {
+            seconde = tempsPause * 60;
+            tempsBool = false;
+        } else {
+            seconde = tempsTravail * 60;
+            tempsBool = true;
+        }
     }
 }
 
-
-function reset(){
-    seconde = 25*60
-    affMin.textContent = 25
-    affSeconde.textContent = "00"
-    but.textContent = "Startttt"
+function reset() {
+    seconde = tempsTravail * 60;
+    affMin.textContent = tempsTravail;
+    affSeconde.textContent = "00";
+    but.textContent = "Start";
     clearInterval(lIntervalDuBoss);
-    bool = true
+    bool = true;
     but.onclick = lancer
 }
 
-//
+function setParam() {
+    parametres.showModal()
+}
+
+
+confirmBtn.addEventListener("click", (event) => {
+    const nouvelleDureeT = inputTravail.value
+    const nouvelleDureeP = inputPause.value
+    if (nouvelleDureeT > 0 && nouvelleDureeP > 0) {
+        tempsTravail = nouvelleDureeT
+        tempsPause = nouvelleDureeP
+        affMin.textContent = tempsTravail
+        reset()
+    }
+    parametres.close()
+});
+
+parametres.addEventListener("close", () => {
+    inputTravail.value = tempsTravail
+    inputPause.value = tempsPause
+
+});
